d4bbc5efcd64972600b7dd4218d8108ec8b78801
crybaby222-0 <josearmandosouza8432@gmail.com>
2026-07-02 23:45:48 -0300

    Api

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // EDIT ME after deploying the backend on Render.
 // Ex.: "https://joe-music-api.onrender.com"
-window.API_BASE = "http://localhost:3000";
+window.API_BASE = "https://back-3-mglw.onrender.com";
